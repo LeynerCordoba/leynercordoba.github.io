73f14b004ae7c1e627f17a644519ba76680d0881
Leyner Cordoba Mosquera <leynercordoba7@gmail.com>
2024-08-16 22:33:02 -0500

    feat: add ci-check action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://leynercordoba.github.io',
+  base: 'leynercordoba.github.io',
   integrations: [tailwind()],
   devToolbar: {
     enabled: false
